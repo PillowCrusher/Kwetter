c3785cd060b0b5a74d2c2a739b56640ade0c6f4a
Rob <robvangastel@hotmail.com>
2017-02-22 00:06:00 +0100

    Update long to Long

--- kwetter/src/main/java/com/robvangastel/kwetter/domain/User.java
+++ kwetter/src/main/java/com/robvangastel/kwetter/domain/User.java
@@ -22,7 +22,7 @@ public class User implements Serializable {
     
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
     
     private Role role;
     
@@ -58,14 +58,14 @@ public class User implements Serializable {
     /**
      * @return the id
      */
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
     /**
      * @param id the id to set
      */
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
