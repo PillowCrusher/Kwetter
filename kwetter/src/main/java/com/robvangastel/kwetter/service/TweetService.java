bd7944ca63166501871bb4ab92d0c23c5fdee561
Rob <robvangastel@hotmail.com>
2017-03-16 14:43:53 +0100

    Add Java Doc Services

--- kwetter/src/main/java/com/robvangastel/kwetter/service/TweetService.java
+++ kwetter/src/main/java/com/robvangastel/kwetter/service/TweetService.java
@@ -37,10 +37,13 @@ public class TweetService {
     @Inject @JPA
     private IUserDao userDao;
 
-    public TweetService() {
-        super();
-    }
-
+    /***
+     *
+     * @param tweet
+     * @return Created tweet
+     * @throws UserException When User doesnt exist
+     * @throws TweetException WHen the message isnt allowed
+     */
 	@RolesAllowed({"USER","ADMINISTRATOR", "MODERATOR"})
     public Tweet create(Tweet tweet) throws UserException, TweetException {
         User user = userDao.findById(tweet.getUser().getId());
@@ -53,6 +56,13 @@ public class TweetService {
 		return null;
     }
 
+    /***
+     *
+     * @param id of Tweet
+     * @param user That preformes the action
+     * @throws TweetException When tweet doesnt exist
+     * @throws UserException When the user isnt found
+     */
 	@RolesAllowed({"USER","ADMINISTRATOR", "MODERATOR"})
     public void delete(long id, User user) throws TweetException, UserException {
         Tweet entity = tweetDao.findById(id);
@@ -65,21 +75,40 @@ public class TweetService {
 		throw new UserException("User not found Exception.");
     }
 
+    /***
+     *
+     * @param id of tweet
+     * @return tweet with the id or null when not found
+     */
 	@PermitAll
     public Tweet findById(long id) {
         return tweetDao.findById(id);
     }
 
+    /***
+     *
+     * @param arg Message of tweet search argument
+     * @return tweets with matching args or null when not found
+     */
 	@PermitAll
     public List<Tweet> findByMessage(String arg) {
         return tweetDao.findByMessage(arg);
     }
 
+    /***
+     *
+     * @param id of the User that made the tweet
+     * @return matching tweets with the same user or null when not found
+     */
 	@PermitAll
     public List<Tweet> findByUser(long id) {
         return tweetDao.findByUser(id);
     }
 
+    /***
+     *
+     * @return All tweets
+     */
 	@PermitAll
     public List<Tweet> findAll() {
         return tweetDao.findAllOrderedByDate();
--- kwetter/src/main/java/com/robvangastel/kwetter/service/UserService.java
+++ kwetter/src/main/java/com/robvangastel/kwetter/service/UserService.java
@@ -30,10 +30,6 @@ public class UserService {
     @Inject @JPA
     private IUserDao dao;
 
-    public UserService() {
-        super();
-    }
-
 	@PermitAll
     public User create(User entity) throws UserException {
         if(dao.findByUsername(entity.getUsername()) == null
@@ -43,18 +39,36 @@ public class UserService {
         return null;
     }
 
+	/***
+	 *
+	 * @param id of the User
+	 * @throws UserException When the User doesnt exist
+     */
 	@RolesAllowed({"ADMINISTRATOR"})
     public void delete(long id) throws UserException {
         User entity = dao.findById(id);
         dao.delete(entity);
     }
 
+	/***
+	 *
+	 * @param entity User to update
+	 * @throws Exception When User doesnt exist
+     */
 	@RolesAllowed({"USER","ADMINISTRATOR", "MODERATOR"})
     public void update(User entity) throws Exception {
         User user = dao.findById(entity.getId());
-        dao.update(user);
+		if(user != null) {
+			dao.update(entity);
+		}
     }
 
+	/***
+	 *
+	 * @param username New username
+	 * @param user User making the request
+	 * @throws UserException When User doesnt exist or username is in use
+     */
 	@RolesAllowed({"USER","ADMINISTRATOR", "MODERATOR"})
     public void updateUsername(String username, User user) throws UserException {
 	    if (dao.findByUsername(username) == null && !username.isEmpty()) {
@@ -63,27 +77,54 @@ public class UserService {
 	    }
     }
 
+	/***
+	 *
+	 * @param role New Role
+	 * @param user User to change the role
+	 * @throws UserException When not authorized or user doesnt exist
+     */
 	@RolesAllowed({"ADMINISTRATOR"})
 	public void updateRole(Role role, User user) throws UserException {
 		user.setRole(role);
 		dao.updateRole(user);
 	}
 
+	/***
+	 *
+	 * @param id of the User
+	 * @return User found or null when not found
+     */
 	@PermitAll
     public User findById(long id) {
         return dao.findById(id);
     }
 
+	/***
+	 *
+	 * @param username of the User
+	 * @return User found or null when not found
+     */
 	@PermitAll
     public User findByUsername(String username) {
         return dao.findByUsername(username);
     }
 
+	/***
+	 *
+	 * @param email of the User
+	 * @return User found or null when not found
+     */
 	@PermitAll
 	public User findByEmail(String email) {
 		return dao.findByEmail(email);
 	}
 
+	/***
+	 *
+	 * @param followingId of the User following
+	 * @param id of the User being followed
+	 * @throws UserException When User doesnt exist
+     */
 	@RolesAllowed({"USER","ADMINISTRATOR", "MODERATOR"})
     public void addFollowing(long followingId, long id) throws UserException {
 	    if(followingId != id) {
@@ -100,11 +141,17 @@ public class UserService {
 	    }
     }
 
+	/***
+	 *
+	 * @param followerId of the User being followed
+	 * @param id of the User making the request
+	 * @throws UserException When the User doesnt exist
+     */
 	@RolesAllowed({"USER","ADMINISTRATOR", "MODERATOR"})
-    public void removeFollowing(long followingId, long id) throws UserException {
-	    if(followingId != id) {
+    public void removeFollowing(long followerId, long id) throws UserException {
+	    if(followerId != id) {
 		    User user = dao.findById(id);
-		    User followingUser = dao.findById(followingId);
+		    User followingUser = dao.findById(followerId);
 		    if(user != null && followingUser != null) {
 			    user.removeFollowing(followingUser);
 			    followingUser.removeFollower(user);
@@ -115,6 +162,10 @@ public class UserService {
 	    }
     }
 
+	/***
+	 *
+	 * @return all Users
+     */
 	@PermitAll
     public List<User> findAll() {
         return dao.findAll();
